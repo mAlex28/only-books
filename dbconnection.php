1d37cef393536467816f69dd84f5a46d69d89cc9
mrash28 <alexthegeek2001@gmail.com>
2021-07-09 23:35:11 +0530

    modified

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,5 @@
 <?php
 
-$url = parse_url(getenv("mysql://b03e9607efe9db:907ce62f@us-cdbr-east-04.cleardb.com/heroku_a5d2e04f2401240?reconnect=true"));
-
 $server = $url["us-cdbr-east-04.cleardb.com"];
 $username = $url["b03e9607efe9db"];
 $password = $url["907ce62f"];
