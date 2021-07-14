c1268b8492da83d535fc0888eb58eb5d0fa726dd
mrash28 <alexthegeek2001@gmail.com>
2021-07-14 23:13:35 +0530

    modified

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Dashboard</title>
+    <title>OnlyBooks</title>
 
     <link rel="shortcut icon" href="../../assets/img/favicon.ico" type="image/x-icon">
 
