fc4ab0bd19ff15212649e6f576f16afd263fc7fc
mrash28 <alexthegeek2001@gmail.com>
2021-07-15 00:20:36 +0530

    logout, profile name and session

--- admin/adminlogin.php
+++ admin/adminlogin.php
@@ -6,7 +6,7 @@ if (isset($_POST['adminLoginBtn'])) {
     $adminLogin = $_POST['username'];
     $passLogin = $_POST['password'];
 
-    $query = "SELECT * FROM admin WHERE email = '$adminLogin' AND password = '$passLogin'";
+    $query = "SELECT * FROM admin WHERE username = '$adminLogin' AND password = '$passLogin'";
     $query_run = mysqli_query($con, $query);
     $usertype = mysqli_fetch_array($query_run);
 
@@ -14,10 +14,6 @@ if (isset($_POST['adminLoginBtn'])) {
 
         $_SESSION['username'] = $adminLogin;
         header('Location: index.php');
-        // } else if ($usertype['userRole'] == 2) {
-
-        //     $_SESSION['username'] = $adminLogin;
-        //     header('Location: ../index.php');
     } else {
 
         $_SESSION['status'] = "Invalid credentials";
