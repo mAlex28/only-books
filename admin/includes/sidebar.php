9a2ef83b403bdbaf78b63b22f9e8f3817bde54f3
mrash28 <alexthegeek2001@gmail.com>
2021-07-14 22:17:36 +0530

    modified

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="index.php" class="brand-link">
-          <img src="dist/img/AdminLTELogo.png" alt="Admin Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+          <img src="../../assets/img/logo_svg.svg" alt="Admin Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
           <span class="brand-text font-weight-light">OnlyBooks Admin</span>
       </a>
 
