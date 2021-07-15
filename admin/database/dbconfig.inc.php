e4e9adccb2e3f3b9d8ef5348f24d0a26ca573f6b
mrash28 <alexthegeek2001@gmail.com>
2021-07-15 23:30:07 +0530

    modified

--- admin/database/dbconfig.inc.php
+++ admin/database/dbconfig.inc.php
@@ -7,11 +7,9 @@ $username = $url["b03e9607efe9db"];
 $password = $url["907ce62f"];
 $db = substr($url["heroku_a5d2e04f2401240"], 1);
 
-// $conn = new mysqli($server, $username, $password, $db);
-$conn = mysqli_connect($server, $username, $password);
-$dbconfig = mysqli_select_db($conn, $db);
+$conn = new mysqli($server, $username, $password, $db);
 
-if ($dbconfig) {
+if ($conn) {
 } else {
     echo '
     <div class="container-fluid">
